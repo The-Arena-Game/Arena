7d52225aaf9af1810de2cd45340a288d6dc59888
boraozenbirkan <b.ozenbirkan@gmail.com>
2024-01-15 18:29:31 +0300

    Project Created & Movement Completed
    Project created and walk, run, jump movements are working.
    
    - Project created from the Third Person template with starter contents.
    - ArenaGameMode Modified and BP created.
    - New Sprint input created and set
    - Files re-ordered.
    
    Closes #2

---
+++ Source/Arena/ArenaCharacter.h
@@ -0,0 +1,88 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "Logging/LogMacros.h"
+#include "ArenaCharacter.generated.h"
+
+class USpringArmComponent;
+class UCameraComponent;
+class UInputMappingContext;
+class UInputAction;
+struct FInputActionValue;
+
+DECLARE_LOG_CATEGORY_EXTERN(LogTemplateCharacter, Log, All);
+DECLARE_LOG_CATEGORY_EXTERN(LogArena, Log, All);
+
+UCLASS(config = Game)
+class AArenaCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	AArenaCharacter();
+
+	/** Returns CameraBoom subobject **/
+	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
+	/** Returns FollowCamera subobject **/
+	FORCEINLINE class UCameraComponent* GetFollowCamera() const { return FollowCamera; }
+
+protected:
+
+	/** Called for movement input */
+	void Move(const FInputActionValue& Value);
+
+	/** Called for sprint input */
+	void Sprint();
+	void StopSprint();
+
+	/** Called for looking input */
+	void Look(const FInputActionValue& Value);
+
+	/** APawn interface */
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	/** To add mapping context */
+	virtual void BeginPlay();
+
+private:
+
+	/** Camera boom positioning the camera behind the character */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	USpringArmComponent* CameraBoom;
+
+	/** Follow camera */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	UCameraComponent* FollowCamera;
+
+	/** MappingContext */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputMappingContext* DefaultMappingContext;
+
+	/** Jump Input Action */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputAction* JumpAction;
+
+	/** Move Input Action */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputAction* MoveAction;
+
+	/** Sprint Input Action */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputAction* SprintAction;
+
+	/** Look Input Action */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputAction* LookAction;
+
+	/** Sprint Speed */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Arena Movement", meta = (AllowPrivateAccess = "true"))
+	float WalkSpeed;
+
+	/** Sprint Speed */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Arena Movement", meta = (AllowPrivateAccess = "true"))
+	float SprintSpeed;
+
+};
---
+++ Source/Arena/ArenaGameMode.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ArenaGameMode.h"
+#include "ArenaCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+AArenaGameMode::AArenaGameMode()
+{
+}
